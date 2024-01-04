23657ab7876e7a6f743c7954f0a65ef891aba4bb
Ayodele Aransiola <leomdevs@gmail.com>
2024-01-04 23:08:34 +0100

    feat: added the command line qr code generator

---
+++ qr-code-genCLI.js
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+import qr from 'qr-image';
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+        message: "Type in your URL: ", 
+        name: "URL" 
+    },
+])
+  .then((answers) => {
+    const url = answers.URL
+    var qr_svg = qr.image(url)
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"))
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
